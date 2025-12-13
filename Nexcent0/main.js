6c26c3ffba18d0f2c990332f510e7315b59691a4
Ramsne13 <amulaliev@gmail.com>
2025-12-13 16:22:43 +0500

    Add files via upload

---
+++ Nexcent0/main.js
@@ -0,0 +1,7 @@
+const buttonMenu = document.querySelector('.mobile-navigation__header-button')
+const mobileNavigation = document.querySelector('.mobile-navigation')
+buttonMenu.addEventListener('click', buttonMenuClicked)
+
+function buttonMenuClicked(){
+    mobileNavigation.classList.toggle('opened')
+}
